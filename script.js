c7c0c191b20f4b4c9c07216dba69c3c807535c10
Travis gun kim <gunget@naver.com>
2023-02-27 21:29:59 +0900

    1. 스크립트에서 css로 windowHeight를 넘겨 body의 Height정하는 작업 개발 중

---
+++ script.js
@@ -0,0 +1,6 @@
+(function () {
+  const windowHeight = window.innerHeight;
+  const windowWidth = window.innerWidth;
+  document.body.style.setProperty("--windowHeight", windowHeight);
+  document.body.style.setProperty("--windowWidth", windowWidth);
+})();
