d95bc6c77ab2d1975db25492b71610e314170f5f
Travis gun kim <gunget@naver.com>
2023-03-05 15:52:23 +0900

    1. 로딩 이미지 첨가 2. lorem Picsum에서 이미지 다 받아오면 로딩 사라지게 설정 3. 받아온 이미지를 upperElem에 넣어주도록 설정 todo. GNV 구분선 필요. 마진이 너무 없음.

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 (function () {
-  // const windowHeight = window.innerHeight;
-  // const windowWidth = window.innerWidth;
-  // document.body.style.setProperty("--windowHeight", windowHeight);
-  // document.body.style.setProperty("--windowWidth", windowWidth);
+  const introImgCT = document.querySelectorAll(".upper");
+  const loadingElem = document.querySelector(".loading");
+
+  const images = [];
+  const imgNums = 4;
+
+  for (let index = 0; index < imgNums; index++) {
+    getFreePics();
+  }
+
+  function getFreePics() {
+    fetch("https://picsum.photos/350/750") //별도 언급 없으면 get방식 요청. promise객체로 만들어서 전송
+      //정상적으로 응답이 완료되면 response를 돌려줌. 안되면 될때까지 pending상태
+      .then((res) => {
+        if (res.url) {
+          let tempHtml = `<img src="${res.url}"/>`;
+          return tempHtml;
+        }
+      })
+      .then((data) => {
+        images.push(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  //초기 로딩화면 후 이미지 다 받아오면 없애기
+  const IntervalId = setInterval(() => {
+    if (images.length == 4) {
+      clearInterval(IntervalId);
+      document.body.classList.remove("before-load");
+      introImgCT.forEach((elem, idx) => {
+        elem.innerHTML = images[idx];
+      });
+      document.body.removeChild(loadingElem);
+    }
+  }, 300);
 })();
